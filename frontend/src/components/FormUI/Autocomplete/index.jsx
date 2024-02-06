4f4cfaaa258e875d167f532acb9105e349f33b35
gaspy2015 <138883972+gaspy2015@users.noreply.github.com>
2024-02-06 13:57:43 +0800

    autocomplete

---
+++ frontend/src/components/FormUI/Autocomplete/index.jsx
@@ -0,0 +1,37 @@
+import { TextField, MenuItem } from "@mui/material";
+import { useField, useFormikContext } from "formik";
+
+const AutocompleteWrapper = ({ name, options, ...otherProps }) => {
+  const { setFieldValue } = useFormikContext();
+  const [field, meta] = useField(name);
+
+  const handleChange = (evt) => {
+    const { value } = evt.target;
+    setFieldValue(name, value);
+  };
+
+  const configSelect = {
+    ...field,
+    ...otherProps,
+    autoComplete: true,
+    variant: "outlined",
+    fullWidth: true,
+    onChange: handleChange,
+  };
+
+  if (meta && meta.touched && meta.error) {
+    configSelect.error = true;
+    configSelect.helperText = meta.error
+  }
+  return (<TextField {...configSelect}>
+        {Object.keys(options).map((item, pos) => {
+            return (
+              <MenuItem key={pos} value={item}>
+                {options[item]}
+              </MenuItem>
+            )
+        })}
+  </TextField>);
+};
+
+export default AutocompleteWrapper;
