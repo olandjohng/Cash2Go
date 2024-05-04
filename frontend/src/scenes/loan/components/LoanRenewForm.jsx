1c98b90ccae787c8b9b4c5ce95f1deb5fa3d77fa
NoemitoJohn <noemito211@gmail.com>
2024-05-04 18:54:27 +0800

    fix

--- frontend/src/scenes/loan/components/LoanRenewForm.jsx
+++ frontend/src/scenes/loan/components/LoanRenewForm.jsx
@@ -84,7 +84,7 @@ export default function LoanRenewForm({dispatcher, popup, loanInitialValue, bank
     
     data = {...data , loan_details : mapLoanDetails} 
     console.log(data)
-    const request = await fetch('http://localhost:8000/loans/renew/', {
+    const request = await fetch('/api/loans/renew/', {
       method : 'post',
       headers: {
         "Content-Type": "application/json",
