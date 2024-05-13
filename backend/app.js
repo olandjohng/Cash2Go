c7273da59142bcc534dcdab6018b882c7d63cf84
NoemitoJohn <noemito211@gmail.com>
2024-05-13 22:58:27 +0800

    payment build

--- backend/app.js
+++ backend/app.js
@@ -13,10 +13,23 @@ const accountCategoryRouter = require('./routes/accountCategory')
 const accountTitleRouter = require('./routes/accountTitle')
 const employeeRouter = require('./routes/employee')
 const paymentRouter = require('./routes/payment');
+const os = require('os');
 
+const getIPv4 = (port) => {
+  const networkInterfaces = os.networkInterfaces()
+  for (const name of Object.keys(networkInterfaces)) {
+    for (const con of networkInterfaces[name]) {
+      if(con.family === 'IPv4' && !con.internal)
+        return `http://${con.address}:${port}`
+    }
+  }
+}
+
+console.log()
 const PORT = 8000
 app.use('/api/public', express.static('public'))
 
+app.use(express.static('dist'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
@@ -35,6 +48,6 @@ app.use('/api/account-category', accountCategoryRouter)
 app.use('/api/account-title', accountTitleRouter)
 app.use('/api/employee', employeeRouter)
 
-app.use('*', express.static('public'))
+app.use('*', express.static('dist'))
 
-app.listen(PORT, () => { console.log(`Server running...`) })
+app.listen(PORT, () => { console.log(`Server running at  \n${getIPv4(PORT)}`) })
