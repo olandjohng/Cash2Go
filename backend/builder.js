99c78b431af6b202ee38bc316ff25f7d67b88586
roland <olandjohngaspar@gmail.com>
2025-09-19 21:09:32 +0800

    local-db-values

--- backend/builder.js
+++ backend/builder.js
@@ -1,24 +1,24 @@
-const knex = require('knex')
-require('dotenv').config()
+const knex = require("knex");
+require("dotenv").config();
 
-const HOST = 'localhost'
-const USER = 'root'
-const PASSWORD = ''
-const DATABASE = 'cash2go'
+const HOST = "localhost";
+const USER = "root";
+const PASSWORD = "!Tested123";
+const DATABASE = "loan_db";
 
 const builder = knex({
-  client: 'mysql2',
+  client: "mysql2",
   connection: {
-    // host : HOST,
-    // user : USER,
-    // password : PASSWORD,
-    // database : DATABASE,
-    port : 3306,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-  }
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE,
+    port: 3306,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_DATABASE,
+  },
 });
 
-module.exports = builder
+module.exports = builder;
