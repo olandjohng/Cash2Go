dff5fbc1ba5d5a232d1bccad2cbbdfbb43475af9
olandjohng <146751009+olandjohng@users.noreply.github.com>
2025-09-20 20:39:05 +0800

    Update builder.js to use env

--- backend/builder.js
+++ backend/builder.js
@@ -9,15 +9,15 @@ const DATABASE = "loan_db";
 const builder = knex({
   client: "mysql2",
   connection: {
-    host: HOST,
-    user: USER,
-    password: PASSWORD,
-    database: DATABASE,
+    // host: HOST,
+    // user: USER,
+    // password: PASSWORD,
+    // database: DATABASE,
     port: 3306,
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   },
 });
 
