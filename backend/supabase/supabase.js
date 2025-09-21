a229c8d028ceae5431d1546b4af210fe663a12f0
roland <olandjohngaspar@gmail.com>
2025-09-21 19:13:14 +0800

    fix supabase values and db

--- backend/supabase/supabase.js
+++ backend/supabase/supabase.js
@@ -2,6 +2,10 @@
 const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config();
 
-// const supabaseClient = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON)
+const SUPABASE_URL = "https://xlwenykwcfwyvcvoceaq.supabase.co";
+const SUPABASE_ANON =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhsd2VueWt3Y2Z3eXZjdm9jZWFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5MjE4NDQsImV4cCI6MjAyODQ5Nzg0NH0.MVfirgXyFO55TMy_LzU5YM97nIMbGa7lYOIAImaGm98";
 
-// module.exports = supabaseClient
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON);
+
+module.exports = supabaseClient;
